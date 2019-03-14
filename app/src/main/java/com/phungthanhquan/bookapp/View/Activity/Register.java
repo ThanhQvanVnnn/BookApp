7d4894b2eb57cc9fb4808aff69a78e1ba7822652
ThanhQvanVnnn <phung.thanhquan1996@gmail.com>
2019-03-14 14:01:32 +0700

    xong giao diện đăng nhập, còn đăng kí

---
+++ app/src/main/java/com/phungthanhquan/bookapp/View/Activity/Register.java
@@ -0,0 +1,15 @@
+package com.phungthanhquan.bookapp.View.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.phungthanhquan.bookapp.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
