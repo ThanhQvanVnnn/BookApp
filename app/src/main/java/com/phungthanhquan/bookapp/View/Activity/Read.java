f420b7657cfd9e3a3e39f9b3b848894cc92cd1b1
ThanhQvanVnnn <phung.thanhquan1996@gmail.com>
2019-03-23 00:42:35 +0700

    test pdfview

--- app/src/main/java/com/phungthanhquan/bookapp/View/Activity/Read.java
+++ app/src/main/java/com/phungthanhquan/bookapp/View/Activity/Read.java
@@ -2,14 +2,75 @@ package com.phungthanhquan.bookapp.View.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.github.barteksc.pdfviewer.PDFView;
 import com.phungthanhquan.bookapp.R;
 
 public class Read extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
+        initControls();
+        pdfView.fromAsset("clean_code.pdf")
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                // allows to draw something on the current page, usually visible in the middle of the screen
+//                .onDraw(onDrawListener)
+//                // allows to draw something on all pages, separately for every page. Called only for visible pages
+//                .onDrawAll(onDrawListener)
+//                .onLoad(onLoadCompleteListener) // called after document is loaded and starts to be rendered
+//                .onPageChange(onPageChangeListener)
+//                .onPageScroll(onPageScrollListener)
+//                .onError(onErrorListener)
+//                .onPageError(onPageErrorListener)
+//                .onRender(onRenderListener) // called after document is rendered for the first time
+//                // called on single tap, return true if handled, false to toggle scroll handle visibility
+//                .onTap(onTapListener)
+//                .onLongPress(onLongPressListener)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+//                .linkHandler(DefaultLinkHandler)
+//                .pageFitPolicy(FitPolicy.WIDTH)
+                .pageSnap(true) // snap pages to screen boundaries
+                .pageFling(false) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();
+        setSupportActionBar(toolbar);
+    }
+
+    private void initControls() {
+        toolbar = findViewById(R.id.toolbar);
+        pdfView = findViewById(R.id.pdfView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_read,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.test:
+                pdfView.setNightMode(true);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
