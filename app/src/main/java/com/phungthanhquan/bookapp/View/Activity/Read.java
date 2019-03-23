7067d3d7ef8c2fe4138c53f699e25f94789da8ca
ThanhQvanVnnn <phung.thanhquan1996@gmail.com>
2019-03-23 14:52:22 +0700

    test thư viện pdf

--- app/src/main/java/com/phungthanhquan/bookapp/View/Activity/Read.java
+++ app/src/main/java/com/phungthanhquan/bookapp/View/Activity/Read.java
@@ -6,23 +6,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.phungthanhquan.bookapp.R;
 
-public class Read extends AppCompatActivity {
+public class Read extends AppCompatActivity implements View.OnClickListener {
 
-    private Toolbar toolbar;
     private PDFView pdfView;
+    private Boolean isUp;
+    private LinearLayout bottom, header;
+    private Toolbar toolbar;
+    Boolean isNightMode,IsVertical;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
         initControls();
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         pdfView.fromAsset("clean_code.pdf")
-                .enableSwipe(true) // allows to block changing pages using swipe
+                .enableSwipe(false) // allows to block changing pages using swipe
                 .swipeHorizontal(true)
-                .enableDoubletap(true)
+                .enableDoubletap(false)
                 .defaultPage(0)
                 // allows to draw something on the current page, usually visible in the middle of the screen
 //                .onDraw(onDrawListener)
@@ -37,25 +47,102 @@ public class Read extends AppCompatActivity {
 //                // called on single tap, return true if handled, false to toggle scroll handle visibility
 //                .onTap(onTapListener)
 //                .onLongPress(onLongPressListener)
-                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .enableAnnotationRendering(true) // render annotations (such as comments, colors or forms)
                 .password(null)
                 .scrollHandle(null)
-                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                .enableAntialiasing(false) // improve rendering a little bit on low-res screens
                 // spacing between pages in dp. To define spacing color, set view background
                 .spacing(0)
-                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+                .autoSpacing(true) // add dynamic spacing to fit each page on its own on the screen
 //                .linkHandler(DefaultLinkHandler)
 //                .pageFitPolicy(FitPolicy.WIDTH)
                 .pageSnap(true) // snap pages to screen boundaries
-                .pageFling(false) // make a fling change only a single page like ViewPager
+                .pageFling(true) // make a fling change only a single page like ViewPager
                 .nightMode(false) // toggle night mode
                 .load();
-        setSupportActionBar(toolbar);
+        bottom.setVisibility(View.INVISIBLE);
+        header.setVisibility(View.INVISIBLE);
+        pdfView.setOnClickListener(this);
+
     }
 
     private void initControls() {
-        toolbar = findViewById(R.id.toolbar);
         pdfView = findViewById(R.id.pdfView);
+        bottom = findViewById(R.id.tool_bottom);
+        header = findViewById(R.id.tool_header);
+        toolbar = findViewById(R.id.toolbar);
+        isUp = false;
+        isNightMode = true;
+        IsVertical = true;
+    }
+
+
+    public void slideHideHeader(View view) {
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+              0  ,  // fromYDelta
+                - view.getHeight());                // toYDelta
+        animate.setDuration(100);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    public void slideShowHeader(View view) {
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                - view.getHeight(),                 // fromYDelta
+                0); // toYDelta
+        animate.setDuration(100);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+
+    public void slideUpBottom(View view) {
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                view.getHeight(),  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(100);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    public void slideDownBottom(View view) {
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight()); // toYDelta
+        animate.setDuration(100);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.pdfView:
+                if (isUp) {
+                    slideDownBottom(bottom);
+                    slideHideHeader(header);
+                } else {
+                    slideShowHeader(header);
+                    slideUpBottom(bottom);
+                }
+                isUp = !isUp;
+                break;
+        }
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 
     @Override
@@ -67,8 +154,25 @@ public class Read extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.test:
-                pdfView.setNightMode(true);
+            case R.id.nightmode:
+                if(isNightMode) {
+                    pdfView.setNightMode(isNightMode);
+                    item.setIcon(R.drawable.ic_nightmode_24dp);
+                   pdfView.loadPages();
+                }
+                else {
+                    pdfView.setNightMode(isNightMode);
+                    item.setIcon(R.drawable.ic_sunny_24dp);
+                    pdfView.loadPages();
+                }
+                isNightMode = !isNightMode;
+                break;
+            case R.id.oritaintion:
+                int currentPage = pdfView.getCurrentPage();
+                pdfView.setSwipeVertical(IsVertical);
+                pdfView.loadPages();
+                pdfView.jumpTo(currentPage,true);
+                IsVertical = !IsVertical;
                 break;
         }
         return super.onOptionsItemSelected(item);
